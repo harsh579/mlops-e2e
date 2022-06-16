9d37e919d67e85d4859d4c2eb659fde23cea2039
harsh579 <harsh.saxena579@live.com>
2022-06-16 05:47:02 +0530

    Update copy_data_manifest.sh

--- scripts/copy_data_manifest.sh
+++ scripts/copy_data_manifest.sh
@@ -5,6 +5,8 @@ set -e
 # Copy the Data Manifest file to the main folder
 
 echo "Data Manifest:"
+echo $CODEBUILD_SRC_DIR_SourceDataOutput
+echo $${CODEBUILD_SRC_DIR_SourceDataOutput}/manifest.json
 cat ${CODEBUILD_SRC_DIR_SourceDataOutput}/manifest.json
 
 echo "Copying Data Manifest to main folder"
